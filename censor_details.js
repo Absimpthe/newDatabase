adc6c8edf30fbde239e9e306eb0955e897d69904
sid-soh <sidneysohzl@gmail.com>
2024-04-10 17:41:21 +0800

    Added email censoring in profile page

---
+++ censor_details.js
@@ -0,0 +1,22 @@
+function censorEmail(email) {
+    let parts = email.split("@"); // Split the email into local part and domain part
+    if (parts.length > 1) {
+        let localPart = parts[0];
+        let censoredPart = '';
+
+        // Check if the local part is longer than 3 characters
+        if (localPart.length > 3) {
+            // Replace the first three characters with asterisks
+            censoredPart = '***' + localPart.substring(3);
+        } else {
+            // If the local part is 3 characters or shorter, censor the entire local part
+            for (let i = 0; i < localPart.length; i++) {
+                censoredPart += '*';
+            }
+        }
+
+        // Reconstruct the email with the domain part
+        return censoredPart + "@" + parts[1];
+    }
+    return email; // Return the original email if it doesn't contain "@"
+}
