48e8003f172e85f78846fa6820548702f4f01a37
sid-soh <sidneysohzl@gmail.com>
2024-04-14 12:01:33 +0800

    WIP on backend of updating tables

--- checkout.php
+++ checkout.php
@@ -9,6 +9,8 @@ $stmt->bind_param("s", $_SESSION['user-id']);
 $stmt->execute();
 $stmt_result = $stmt->get_result();
 
+// get today's date here
+
 if ($stmt_result->num_rows > 0) {
     $user_data = $stmt_result->fetch_assoc();
     
@@ -16,7 +18,17 @@ if ($stmt_result->num_rows > 0) {
         
         if (!empty($_SESSION['cart'])) {
 
-            // need to update orders table first
+            // Update orders table
+            $add_order = $con->prepare("INSERT INTO orders (CustomerID, TotalPrice, Date, OrderStatus, Payment Status) VALUES (?, ?, ?, Pending, Pending");
+            $add_order->bind_param("ids", $_SESSION['user_id'], $_SESSION['Total'], $date);
+            $add_order->execute();
+
+            $sql = $con->prepare("select OrderID from orders where CustomerID = ?");
+            $sql->bind_param("s", $_SESSION['user_id']);
+            $sql->execute();
+            $sql_result = $sql->get_result();
+            $order_id = $sql_result->fetch_assoc();
+
             foreach ($_SESSION['cart'] as $item) {
                 // Get the item code
                 $find_item = $con->prepare("select ItemCode from items where ItemName = ?");
@@ -25,9 +37,9 @@ if ($stmt_result->num_rows > 0) {
                 $find_item_result = $find_item->get_result();
                 $item_code = $find_item_result->fetch_assoc();
 
-                $add_order = $con->prepare("INSERT INTO orderitems (ItemCode, ItemQuantity, SubtotalPrice) VALUES (?, ?, ?)");
-                $add_order->bind_param("sid", $item_code['ItemCode'], $item['quantity'], $item['price']);
-                $add_order->execute();
+                $add_order_items = $con->prepare("INSERT INTO orderitems (orderID, ItemCode, ItemQuantity, SubtotalPrice) VALUES (?, ?, ?, ?)");
+                $add_order_items->bind_param("isid", $order_id['orderID'], $item_code['ItemCode'], $item['quantity'], $item['price']);
+                $add_order_items->execute();
             }
 
 
