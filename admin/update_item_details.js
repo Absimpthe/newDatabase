cc7cb1a8aa5a1c5f81ec5f291d2d99b288fdaf55
sid-soh <sidneysohzl@gmail.com>
2024-04-25 01:59:45 +0800

    [WIP] Feature: update item details

--- admin/update_item_details.js
+++ admin/update_item_details.js
@@ -3,36 +3,42 @@ document.addEventListener("DOMContentLoaded", function() {
     const saveButton = document.getElementById('save-btn');
 
     saveButton.addEventListener('click', function() {
-        const items = [];
+        const items = []; 
         document.querySelectorAll('.menu-container').forEach(container => {
-            const itemCode = container.querySelector('#item-code').value;
-            const itemName = container.querySelector('#item-name').value;
-            const price = container.querySelector('#item-price').value;
-            const availability = container.querySelector('#stock').value;
+            container.querySelectorAll('.menu-item').forEach(item => {
+                var itemCode = item.querySelector('.item-code').value;
+                var itemName = item.querySelector('.item-name').value;
+                var price = item.querySelector('.item-price').value;
+                var availability = item.querySelector('.stock').value;
 
-            items.push({
-                itemCode: itemCode,
-                itemName: itemName,
-                price: price,
-                availability: availability
+                items.push({
+                    itemCode: itemCode,
+                    itemName: itemName,
+                    price: price,
+                    availability: availability
+                });
             });
         });
 
-        fetch('/admin/update_items.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ items: items })
-        })
-        .then(response => response.json())
-        .then(data => {
-            alert('Update successful!');
-        })
-        .catch(error => {
-            console.error('Error updating items:', error);
-            alert('Failed to update items.');
-        });
+        if (items.length > 0) {
+            fetch('/admin/update_items.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ items: items })
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Update successful');
+                alert('All items updated successfully!');
+            })
+            .catch(error => {
+                console.error('Error updating items:', error);
+                alert('Failed to update items.');
+            });
+        } else {
+            alert('No items to update.');
+        }
     });
-
 });
